1c7c9451f14953ba1d3369f722434328f3e9c01a
Abdulrehman Afzal <you@example.com>
2026-02-11 18:55:04 +0500

    first commit

--- app/page.js
+++ app/page.js
@@ -2,15 +2,16 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Navbar from "./page/navbar/page";
 import Hero from "./page/home/page";
-
-
+import FeaturesSection from "./page/feature/page";
+import HowItWorks from "./page/How-its-work/page";
+import { Import } from "lucide-react";
 export default function Home() {
   return (
     <>
      <Navbar />
-    
     <Hero />
-    
+    <FeaturesSection/>
+<HowItWorks/>    
     
     </>
   );
